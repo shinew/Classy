4b4449c0fae5fd386661bef281493eee9a724a7a
Shine Wang <shinewang@live.ca>
2013-07-19 15:08:45 -0400

    improved queries

---
+++ Testing/queries.py
@@ -0,0 +1,17 @@
+import urllib
+from HTMLParser import HTMLParser
+
+a = []
+class MyHTMLParser(HTMLParser):
+	def handle_data(self, data):
+		if len(data.strip()) != 0:
+			a.append(data.strip())
+
+params = urllib.urlencode({'sess' : '1139', 'subject' : 'AFM'})
+#params = urllib.urlencode({'sess' : '2320', 'subject' : 'AFM'})
+
+f = urllib.urlopen("http://www.adm.uwaterloo.ca/cgi-bin/cgiwrap/infocour/salook.pl", params)
+htm = f.read() #htm contains the Accounting (AFM) courses for the Fall 2013 (1139) session
+
+parser = MyHTMLParser()
+parser.feed(htm)
--- Testing/querySchedule.py
+++
@@ -1,4 +0,0 @@
-import urllib
-params = urllib.urlencode({'sess' : '1139', 'subject' : 'AFM'})
-f = urllib.urlopen("http://www.adm.uwaterloo.ca/cgi-bin/cgiwrap/infocour/salook.pl", params)
-print f.read() #this now primts the AFM page for the Fall 2013 session

c87d5235c1bf1607e8de4f2f17f1c467756945f8
Shine Wang <shinewang@live.ca>
2013-07-21 21:08:14 -0400

    yay, caching of RMP.com success

--- Main/main.py
+++ Main/main.py
@@ -1,7 +1,9 @@
+import time
 from webParser import WebParser
 from matcher import Matcher
 from profParser import RateMyProfParser
 
+programStartTime = time.time()
 sessionString = "fall 2013"
 userCourses = ["math 135", "math 137", "cs 145", "afm 101", "econ 101"]
 
@@ -12,25 +14,30 @@ for courseName in userCourses:
 
 # rateMyProfs
 for i, course in enumerate(courses):
-    print "Doing course", i+1
-    for slot in course.lectures:
+    print "Course {}/{}".format(str(i+1), len(courses))
+
+    for j, slot in enumerate(course.lectures):
+        print "Lecture {}/{}".format(str(j+1), len(course.lectures))
         ret = RateMyProfParser(slot.instructor).getInfo()
         if ret:
             slot.numRatings, slot.quality, slot.easiness = ret
-    for slot in course.tutorials:
+
+    for j, slot in enumerate(course.tutorials):
+        print "Tutorial {}/{}".format(str(j+1), len(course.tutorials))
         ret = RateMyProfParser(slot.instructor).getInfo()
         if ret:
             slot.numRatings, slot.quality, slot.easiness = ret
 
 # now, we sort by ease, then quality, then number of ratings
 for course in courses:
-    course.lectures.sort(key = lambda x: (x.easiness, x.quality,
-                                          x.numRatings), reverse=True)
-
+    course.lectures.sort(key=lambda x: (x.easiness, x.quality,
+                                        x.numRatings), reverse=True)
 
 # then we generate non-time-conflicting schedules
 match = Matcher(courses)
 generator = match.matching()
+
+print "Time elapsed: {:.3f}".format(time.time() - programStartTime)
 
 for schedule in generator:
     for slot in schedule:

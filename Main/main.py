629e030e925cbc63c30639b358bc92c4cd7015e6
Shine Wang <shinewang@live.ca>
2013-07-21 21:43:54 -0400

    better UI

--- Main/main.py
+++ Main/main.py
@@ -3,46 +3,90 @@ from webParser import WebParser
 from matcher import Matcher
 from profParser import RateMyProfParser
 
-programStartTime = time.time()
+
+# default values left for debugging
 sessionString = "fall 2013"
-userCourses = ["math 135", "math 137", "cs 145", "afm 101", "econ 101"]
-
+# userCourses = ["math 135", "math 137", "cs 145", "afm 101", "econ 101"]
+userCourses = []
 courses = []
-for courseName in userCourses:
-    tmp = WebParser(courseName, sessionString).run()
-    courses.append(tmp)
+generator = None
+myTime = time.time()
 
-# rateMyProfs
-for i, course in enumerate(courses):
-    print "Course {}/{}".format(str(i+1), len(courses))
+def programTime():
+    print "This program took {:.2f}s to complete.".format(time.time()
+                                                          - myTime)
 
-    for j, slot in enumerate(course.lectures):
-        print "Lecture {}/{}".format(str(j+1), len(course.lectures))
-        ret = RateMyProfParser(slot.instructor).getInfo()
-        if ret:
-            slot.numRatings, slot.quality, slot.easiness = ret
+def getUserInfo(userCourses):
+    print "HI there! I am Classy, and I'll optimize your course selection."
+    sessionString = raw_input("Please tell me which session are in " \
+            "(e.g. 'fall 2013' without quotes): ")
+    cour = ""
+    while cour.strip() != "0":
+        cour = raw_input("Please add a course (e.g. 'math 145' " \
+                "without quotes).\nEnter '0' (without quotes) to" \
+                " stop adding: ")
+        userCourses.append(cour)
+    userCourses.pop()  # the last '0'
 
-    for j, slot in enumerate(course.tutorials):
-        print "Tutorial {}/{}".format(str(j+1), len(course.tutorials))
-        ret = RateMyProfParser(slot.instructor).getInfo()
-        if ret:
-            slot.numRatings, slot.quality, slot.easiness = ret
+def queryUniversity(userCourses, courses):
+    print "Currently querying adm.uwaterloo.ca..."
+    for courseName in userCourses:
+        tmp = WebParser(courseName, sessionString).run()
+        courses.append(tmp)
 
-# now, we sort by ease, then quality, then number of ratings
-for course in courses:
-    course.lectures.sort(key=lambda x: (x.easiness, x.quality,
-                                        x.numRatings), reverse=True)
+def queryRateMyProfessors(courses):
+    print "Currently querying ratemyprofessors.com..."
 
-# then we generate non-time-conflicting schedules
-match = Matcher(courses)
-generator = match.matching()
+    for i, course in enumerate(courses):
+        print "Process: Course {}/{}".format(str(i+1), len(courses))
 
-print "Time elapsed: {:.3f}".format(time.time() - programStartTime)
+        for j, slot in enumerate(course.lectures):
+            #print "Lecture {}/{}".format(str(j+1), len(course.lectures))
+            ret = RateMyProfParser(slot.instructor).getInfo()
+            if ret:
+                slot.numRatings, slot.quality, slot.easiness = ret
 
-for schedule in generator:
+        for j, slot in enumerate(course.tutorials):
+            #print "Tutorial {}/{}".format(str(j+1), len(course.tutorials))
+            ret = RateMyProfParser(slot.instructor).getInfo()
+            if ret:
+                slot.numRatings, slot.quality, slot.easiness = ret
+
+def postProcessing(courses):
+    # sort by ease, then quality, then number of ratings
+    for course in courses:
+        course.lectures.sort(key=lambda x: (x.easiness, x.quality,
+                                            x.numRatings), reverse=True)
+
+    # then we generate non-time-conflicting schedules
+
+def scheduleGeneration(generator):
+    print "All calculations complete!"
+    print "You will be given a chance to save your selected schedule" \
+          " later."
+
+    generator = Matcher(courses).matching()
+
+    for schedule in generator:
+        if schedule is None:
+            break
+        for slot in schedule:
+            print slot
+        print
+        inp = raw_input("enter 'q' to quit and save your file;" \
+                        " enter 'n' to see the next schedule: ")
+        if inp.lower() == 'q':
+            break
+    outputFile = raw_input("Where would you like to save this schedule?" \
+          " Please enter a file name: ")
+    f = open(outputFile, "w")
     for slot in schedule:
-        print slot
-    print
-    inp = raw_input("enter q to quit; enter n to continue: ")
-    if inp.lower() == 'q':
-        break
+        f.write(repr(slot) + "\n")
+    f.close()
+
+getUserInfo(userCourses)
+queryUniversity(userCourses, courses)
+queryRateMyProfessors(courses)
+postProcessing(courses)
+# programTime()
+scheduleGeneration(generator)

a2e43dfddada6b045be236db62b54b79dde6c56a
Shine Wang <shinewang@live.ca>
2013-07-23 13:58:41 -0400

    yay, successful retrieval

--- Testing/getCourses.py
+++ Testing/getCourses.py
@@ -1,3 +1,20 @@
+"""
+Copyright 2013 Shine Wang
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
 import urllib
 from HTMLParser import HTMLParser
 
@@ -15,6 +32,7 @@ class CustomHTMLParser(HTMLParser):
 
 names = []  # contains "AFM", "CS", etc.
 with open("names.txt", "r") as f:
+    # the names.txt contains all the possible prefixes like AFM, CS, etc.
     names = f.read().split()
 
 requestURL = "http://www.adm.uwaterloo.ca/cgi-bin/" \
@@ -22,6 +40,7 @@ requestURL = "http://www.adm.uwaterloo.ca/cgi-bin/" \
 
 allIDs = []  # contains "AFM 101", "CS 145", etc.
 for name in names:
+    print "Currently doing", name + "..."
     webData = []
     params = urllib.urlencode({"sess": "1139", "subject": name})
     page = urllib.urlopen(requestURL, params)
@@ -32,6 +51,6 @@ for name in names:
             allIDs.append(webData[i+9] + " " + webData[i+11])
 
 # write to file
-with open("allIDs.txt", "w") as f:
+with open("allIDs", "w") as f:
     for ID in allIDs:
         f.write(ID + "\n")
